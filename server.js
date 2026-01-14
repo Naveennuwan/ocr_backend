09fea32b9b43c485ad9e87cc7ce9d623db7fe9e7
Naveennuwan <naveen@infinit.lk>
2026-01-14 13:59:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: 'https://smartocr.netlify.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
